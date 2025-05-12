e6359c32916926c3c373b37803bd63f4d5a751a7
lksoumon <99751496+lksoumon@users.noreply.github.com>
2025-05-12 09:11:26 -0400

    Update carimbadorGED.user.js

--- carimbadorGED.user.js
+++ carimbadorGED.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Carimbador GED
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.61
 // @description  try to take over the world!
 // @author       Lucas Monteiro
 // @require https://code.jquery.com/jquery-3.6.0.min.js
@@ -40,7 +40,7 @@ var tecs = [
         var searchText1 = "Secretário(a)";
         var searchText2 = "Diretor(a)";
         var secretario = " LUCAS DE SOUZA MONTEIRO \r\n Secretário Escolar \r\n Portaria nº1.677/2023/GS/SEDUC/MT ";
-        var diretor = " RODRIGO LEANDRO LEMES GONÇALVES \r\n Diretor Escolar \r\n Portaria nº1.678/2023/GS/SEDUC/MT ";
+        var diretor = " ANA PATRICIA DOS SANTOS \r\n Diretora Escolar \r\n Portaria SEDUC/00421/2025 ";
         var tecnico = TAE+" \r\n Téc. Administrativo Escolar \r\n E.E. Major Otávio Pitaluga ";
         var found;
         var modo;
